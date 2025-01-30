45e4fa36e9557f136aef71920ce799737b77fbea
Misxy <supakorn.dev@outlook.com>
2025-01-31 00:37:10 +0700

    Initialized the codebase.

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Button from 'primevue/button';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router).use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: 'system',
+      cssLayer: false,
+    },
+  },
+});
+app.mount('#app');
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { Button } from 'primevue';
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <Button label="Submit" />
+</template>
