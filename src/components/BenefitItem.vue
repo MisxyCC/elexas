d9e8adc03ede5433b27b83ff1179300d6371494e
513747 <supakorn.chu@pea.co.th>
2025-06-04 14:16:36 +0700

    Updated the routing from main page to exercise page.

---
+++ src/components/BenefitItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="flex flex-col items-center text-center p-8 bg-[#F9FAF8] rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300"
+  >
+    <div class="text-[#30e89e] mb-6">
+      <span class="material-icons-outlined" style="font-size: 64px">{{ cardItem.iconName }}</span>
+    </div>
+    <h3 class="text-2xl font-semibold text-[#1A2E25] mb-3">{{ cardItem.title }}</h3>
+    <p class="text-[#4B5563] text-lg font-light leading-relaxed">
+      {{ cardItem.description }}
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+import type { CardDetail } from '@/models/CardDetail';
+
+const cardItem = defineModel<CardDetail>({
+  required: true,
+});
+</script>
--- src/components/VideoItem.vue
+++ src/components/VideoItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <Card class="">
+  <Card class="rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
     <template #header>
       <img alt="user header" src="@/assets/landing-img.png" class="w-full h-64 object-cover" />
     </template>
-    <template #title>Advanced Card</template>
+    <template #title>โปรแกรมออกกำลังกาย</template>
     <template #subtitle>Card subtitle</template>
     <template #content>
       <p class="m-0">
@@ -19,7 +19,7 @@
       <div class="flex flex-wrap justify-center items-center mt-1">
         <button
           class="flex w-auto cursor-pointer items-center justify-center overflow-hidden rounded-xl h-14 px-8 mx-auto bg-[#30e89e] text-[#1A2E25] text-xl md:text-2xl font-semibold leading-normal tracking-wide border-2 border-transparent hover:border-[#30e89e] transition-all duration-300 ease-in-out transform hover:scale-105"
-          disabled
+          @click="router.push('/exercise')"
         >
           <span class="truncate">ออกกำลังกาย</span>
         </button>
@@ -27,4 +27,10 @@
     </template>
   </Card>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { VideoDetail } from '@/models/VideoDetail';
+import { useRouter } from 'vue-router';
+
+const model = defineModel<VideoDetail>();
+const router = useRouter();
+</script>
